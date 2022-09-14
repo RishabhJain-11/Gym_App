752a8b489ce830d2109df06b069e3b1e7e12ddb2
Rishabh Jain <jainrishabh71011@gmail.com>
2022-09-14 21:05:13 +0530

    ..

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,4 @@
+
 import React from 'react'
 import { Box, Stack, Typography } from '@mui/material';
 import Logo from '../assets/images/Logo-1.png';
@@ -9,13 +10,10 @@ const Footer = () => {
         <img src={Logo} alt="logo" style={{ width: '200px', height: '41px' }} />
       </Stack>
       <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">
-        Made with ❤️ by Rishabh Jain
+        Copyright © 2022 Rishabh Jain. 
       </Typography>
     </Box>
   )
 }
-
-
-
 
 export default Footer
